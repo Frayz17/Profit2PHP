ad8c955f67724c8c5c85b281056f960b011829fe
Frayz17 <andrydmit@gmail.com>
2016-09-20 23:15:05 +0300

    Starts git rep and add class Db and create autoloader

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    require __DIR__ . '/autoload.php';
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $db = new \App\Db();
+        ?>
+    </body>
+</html>
